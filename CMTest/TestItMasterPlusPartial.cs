e8b098815bb0601a79931a5dd9c5af821c66b563
HuangLuffy <30331150+HuangLuffy@users.noreply.github.com>
2019-08-22 16:03:18 +0800

    123
    123

--- CMTest/TestItMasterPlusPartial.cs
+++ CMTest/TestItMasterPlusPartial.cs
@@ -10,17 +10,15 @@ namespace CMTest
 {
     public partial class TestIt
     {
-        IDictionary<string, Func<dynamic>> Options_MasterPlus_Tests_With_Funcs = new Dictionary<string, Func<dynamic>>();
+        readonly IDictionary<string, Func<dynamic>> _optionsMasterPlusTestsWithFuncs = new Dictionary<string, Func<dynamic>>();
 
         private void AssembleMasterPlusPlugInOutTests()
         {
-            if (Options_MasterPlus_Tests_With_Funcs.Count() == 0)
-            {
-                //Select Back that will trigger this function again, so try-catch.
-                Options_MasterPlus_Tests_With_Funcs.Add(MasterPlusTestFlows.Test_Names.OPTION_LAUNCH_TEST, Flow_MasterPlus_LaunchTest);
-                Options_MasterPlus_Tests_With_Funcs.Add(UtilCmd.OPTION_SHOW_MENU_AGAIN, _CMD.MenuShowAgain);
-                Options_MasterPlus_Tests_With_Funcs.Add(UtilCmd.OPTION_BACK, _CMD.MenuGoback);
-            }
+            if (_optionsMasterPlusTestsWithFuncs.Any()) return;
+            //Select Back that will trigger this function again, so try-catch.
+            _optionsMasterPlusTestsWithFuncs.Add(MasterPlusTestFlows.Test_Names.OPTION_LAUNCH_TEST, Flow_MasterPlus_LaunchTest);
+            _optionsMasterPlusTestsWithFuncs.Add(UtilCmd.OPTION_SHOW_MENU_AGAIN, _CMD.MenuShowAgain);
+            _optionsMasterPlusTestsWithFuncs.Add(UtilCmd.OPTION_BACK, _CMD.MenuGoback);
         }
         private dynamic Flow_MasterPlus_LaunchTest()
         {
